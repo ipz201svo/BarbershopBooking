fc2bd457196614ffcd06b3db023e18e6f076eb97
Vladyslav <ipz201_svo@student.ztu.edu.ua>
2022-08-16 16:53:44 +0300

    Add project files.

---
+++ Domain/Common/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace BarbershopBooking.Domain.Common;
+
+public abstract class AuditableEntity
+{
+    public DateTime Created { get; init; }
+
+    public string? CreatedBy { get; init; }
+
+    public DateTime? LastModified { get; init; }
+
+    public string? LastModifiedBy { get; init; }
+}
---
+++ Domain/_Imports.cs
@@ -0,0 +1,1 @@
+﻿global using BarbershopBooking.Domain.Common;
